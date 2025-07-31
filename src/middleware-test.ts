32d69895b1d8a95ae3adfb4ff686e62e098f1068
Victor Bello <imuetinyanbello@gmail.com>
2025-07-31 16:18:17 +0100

    removed middleware

---
+++ src/middleware-test.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+
+  const res = NextResponse.next();
+
+  res.headers.set("Access-Control-Allow-Credentials", "true");
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date"
+  );
+  return res;
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
